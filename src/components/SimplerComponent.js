e1b429eca5ee883c6adcd3002066cff38b810d59
nawal abdualghani <nawalabdualghani@gmail.com>
2019-12-09 13:43:04 +0000

    Automatically backed up by Learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,31 @@
 // Code SimplerComponent Here
+import React, { Component } from "react";
+
+export default class SimpleComponent extends Component {
+  constructor() {
+   super();
+
+   this.state = {
+     mood:'happy'
+   };
+ }
+ handleClick = () => {
+   if(this.state.mood==happy)
+   this.setState({
+     this.mood:'sad'
+   })
+else {
+  this.setState({
+    this.mood:'happy'
+  })
+}
+  }
+  render() {
+    return (
+      <div >
+      <button onClick={this.handleClick}>click</button>
+      {this.state}
+      </div>
+    );
+  }
+}
