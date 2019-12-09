3158e231c42a3248322234c36c87436497ecdc27
nawal abdualghani <nawalabdualghani@gmail.com>
2019-12-09 13:29:34 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,14 @@
 // Code SimpleComponent Here
+import React, { Component } from "react";
+
+export default class SimpleComponent extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.posts.map(post => (
+          <div>{post.title}</div>
+        ))}
+      </div>
+    );
+  }
+}
