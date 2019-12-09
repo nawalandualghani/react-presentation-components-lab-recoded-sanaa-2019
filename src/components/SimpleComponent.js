326f2263ecae033a866d42a840d980f616a1b47f
nawal abdualghani <nawalabdualghani@gmail.com>
2019-12-09 13:30:16 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -2,6 +2,13 @@
 import React, { Component } from "react";
 
 export default class SimpleComponent extends Component {
+  constructor() {
+   super();
+
+   this.state = {
+     posts: []
+   };
+ }
   render() {
     return (
       <div>
